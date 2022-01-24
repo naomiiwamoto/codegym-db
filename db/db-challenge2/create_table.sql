8755fbab8003e1bf98793b0a2c7acbc67fb0b6cd
naomikohara <naaaomi.0717@gmail.com>
2022-01-24 15:10:08 +0900

    db-課題2修正版1

--- db/db-challenge2/create_table.sql
+++ db/db-challenge2/create_table.sql
@@ -9,31 +9,31 @@ mobile_phone_number VARCHAR(13),
 work_phone_number VARCHAR(13),
 updated_at datetime not null,
 created_at datetime not null,
-is_deleted tinyint(1),
+is_deleted tinyint(1) default 0 not null,
 PRIMARY KEY(id)
  );
 
 -- チャットルーム
-CREATE TABLE chat_room
+CREATE TABLE chat_rooms
 (id INT AUTO_INCREMENT not null,
-user_id INT not null,
+create_user_id INT not null,
 chat_name VARCHAR(100) not null,
-chat_description VARCHAR(100),
-file_send_permit_flag tinyint(1),
-direct_chat_flag tinyint(1),
+chat_description VARCHAR(1000),
+file_send_permit_flag tinyint(1) default 0 not null,
+direct_chat_flag tinyint(1) default 0 not null,
 created_at datetime not null,
 update_user_id INT not null,
 updated_at datetime not null,
-is_deleted tinyint(1),
+is_deleted tinyint(1) default 0 not null,
 PRIMARY KEY(id),
-FOREIGN KEY (user_id)
+FOREIGN KEY (create_user_id)
 REFERENCES users (id),
 FOREIGN KEY (update_user_id)
 REFERENCES users (id)
 );
 
 -- チャット接続
-CREATE TABLE chat_connection
+CREATE TABLE chat_connections
 (id INT AUTO_INCREMENT not null,
 user_id INT not null,
 chat_room_id INT not null,
@@ -42,47 +42,47 @@ PRIMARY KEY(id),
 FOREIGN KEY (user_id)
 REFERENCES users (id),
 FOREIGN KEY (chat_room_id)
-REFERENCES chat_room(id)
+REFERENCES chat_rooms(id)
 );
 
 -- 投稿
 CREATE TABLE posts
 (id INT AUTO_INCREMENT not null,
-user_id INT not null,
+create_user_id INT not null,
 chat_room_id INT not null,
 attachment_name VARCHAR(100) not null,
-post_content VARCHAR(100),
+post_content VARCHAR(1000) not null,
 post_time datetime not null,
-update_user_id INT,
+update_user_id INT not null,
 updated_at datetime not null,
-is_deleted tinyint(1),
+is_deleted tinyint(1) default 0 not null,
 PRIMARY KEY(id),
-FOREIGN KEY (user_id)
+FOREIGN KEY (create_user_id)
 REFERENCES users (id),
 FOREIGN KEY (chat_room_id)
-REFERENCES  chat_room(id),
+REFERENCES chat_rooms(id),
 FOREIGN KEY (update_user_id)
 REFERENCES users (id)	
 );
 
 -- タスク
-CREATE TABLE todo
+CREATE TABLE tasks
 (id INT AUTO_INCREMENT not null,
-user_id INT not null,
+create_user_id INT not null,
 chat_room_id INT not null,
 task_rep_id INT not null,
-task_detail VARCHAR(100) not null,
+task_detail VARCHAR(1000) not null,
 time_limit datetime  not null,
-complete_flag tinyint(1),
+complete_flag tinyint(1) default 0 not null,
 created_at datetime not null,
 update_user_id INT not null,
 updated_at datetime not null,
-is_deleted tinyint(1),
+is_deleted tinyint(1) default 0 not null,
 PRIMARY KEY(id),
-FOREIGN KEY (user_id)
+FOREIGN KEY (create_user_id)
 REFERENCES users(id),
 FOREIGN KEY (chat_room_id)
-REFERENCES chat_room(id),
+REFERENCES chat_rooms(id),
 FOREIGN KEY (task_rep_id)
 REFERENCES users(id),
 FOREIGN KEY (update_user_id)
