70c4746db329c4722022b8a626f1af0ca8e41e98
naomikohara <naaaomi.0717@gmail.com>
2022-01-31 16:47:34 +0900

    db-課題3 問題番号011～014修正版

--- db/db-challenge3/db-012/db-012.sql
+++ db/db-challenge3/db-012/db-012.sql
@@ -1,4 +1,8 @@
+BEGIN;
+
 UPDATE chat_rooms 
-SET file_send_permit_flag =0,updated_at=CURRENT_TIMESTAMP 
+SET file_send_permit_flag =0,updated_at=CURRENT_TIMESTAMP,update_user_id = 1
 WHERE is_deleted=0 
 AND id IN (SELECT chat_room_id FROM chat_connections WHERE chat_room_id NOT IN (SELECT chat_room_id FROM chat_connections WHERE user_id=1));
+
+COMMIT;
--- db/db-challenge3/db-014/db-014.sql
+++ db/db-challenge3/db-014/db-014.sql
@@ -1,5 +1,9 @@
+COMMIT;
+
 DELETE chat_connections 
 FROM chat_connections 
 INNER JOIN users ON 
 chat_connections.user_id = users.id 
 WHERE users.is_deleted =1;
+
+COMMIT;
